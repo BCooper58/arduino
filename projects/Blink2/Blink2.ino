e6a7e4421f46ef5b16c71260a969e2d683f73961
Brenden Cooper <brc51101@gmail.com>
2018-01-25 08:51:17 -0800

    kek

---
+++ projects/Blink2/Blink2.ino
@@ -0,0 +1,51 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+ digitalWrite(13, HIGH);
+ delay(1000);
+ digitalWrite(13, LOW);
+ delay(1000);
+ digitalWrite(13, HIGH);
+ delay(1000);
+ digitalWrite(13, LOW);
+ delay(1000);
+ digitalWrite(13, HIGH);
+ delay(500);
+ digitalWrite(13, LOW);
+ delay(500);
+ digitalWrite(13, HIGH);
+ delay(75);
+ digitalWrite(13, LOW);
+ delay(75);
+ digitalWrite(13, HIGH);
+ delay(75);
+ digitalWrite(13, LOW);
+ delay(75);
+ digitalWrite(13, HIGH);
+ delay(75);
+ digitalWrite(13, LOW);
+ delay(75);
+ digitalWrite(13, HIGH);
+ delay(75);
+ digitalWrite(13, LOW);
+ delay(75);
+ digitalWrite(13, HIGH);
+ delay(200);
+ digitalWrite(13, LOW);
+ delay(200);
+ digitalWrite(13, HIGH);
+ delay(200);
+ digitalWrite(13, LOW);
+ delay(200);
+ digitalWrite(13, HIGH);
+ delay(200);
+ digitalWrite(13, LOW);
+ delay(200);
+ digitalWrite(13, HIGH);
+ delay(200);
+ digitalWrite(13, LOW);
+ delay(200);
+}
